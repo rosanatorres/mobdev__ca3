1e8ab0ee81c041fce46e32e1b9dee3d18aa93f26
Rosana Torres <98158262+rosanatorres@users.noreply.github.com>
2022-11-12 14:18:33 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.mobdev_ca3',
+  appName: 'mobdev_ca3',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
