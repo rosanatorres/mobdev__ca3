dd8581083dc776e2011155f667a4cfb692f42213
Rosana Torres <98158262+rosanatorres@users.noreply.github.com>
2022-11-13 10:10:08 +0000

    small changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,37 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)},
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: 'characters',
+    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
+  },
+  {
+    path: 'character-detail',
+    loadChildren: () => import('./pages/character_details/character_details.module').then( m => m.CharacterDetailsPageModule)
+  },
+  {
+    path: 'episodes',
+    loadChildren: () => import('./pages/episodes/episodes.module').then( m => m.EpisodesPageModule)
+  },
+  {
+    path: 'episode_details',
+    loadChildren: () => import('./pages/episode_details/episode_details.module').then( m => m.EpisodeDetailsPageModule)
+  },
+  {
+    path: 'quotes',
+    loadChildren: () => import('./pages/quotes/quotes.module').then( m => m.QuotesPageModule)
+  },
+  {
+    path: 'deaths',
+    loadChildren: () => import('./pages/deaths/deaths.module').then( m => m.DeathsPageModule)
   }
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
